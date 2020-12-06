8e8a808e7d8b3ba7fa174008ad2a225c8b60dda2
jaydip <jaydipsavaliya1310@gmail.com>
2020-12-06 10:00:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/videoviewdemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.videoviewdemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    VideoView videoView;
+    MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaController controller = new MediaController(this);
+        button = findViewById(R.id.VideoButton);
+        videoView = findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.samplevideo);
+        mediaController.playSoundEffect(View.SOUND_EFFECTS_ENABLED);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Video"),205);
+            }
+        });
+    }
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Video.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return "jay";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 205 && resultCode == RESULT_OK){
+            Log.e("jaydip",data.getData().getPath());
+            videoView.setVideoURI(data.getData());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/videoviewdemo/imageActivity.java
@@ -0,0 +1,132 @@
+package com.example.videoviewdemo;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class imageActivity extends AppCompatActivity {
+
+    Button ImageButton,ImageButton2;
+    ImageView imageView;
+    Bitmap img1,img2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        ImageButton = findViewById(R.id.ImageButton);
+        imageView = findViewById(R.id.imageView);
+        ImageButton2 = findViewById(R.id.ImageButton2);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,205);
+
+            }
+        });
+        ImageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                overPrint();
+                drawTextToBitmap(getApplicationContext(),img1,"jay");
+//                Intent intent = new Intent();
+//                intent.setType("image/*");
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(intent,206);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 205 && resultCode == RESULT_OK){
+            try {
+                img1 = MediaStore.Images.Media.getBitmap(getContentResolver(),data.getData());
+                imageView.setImageBitmap(img1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        if(requestCode == 206 && resultCode == RESULT_OK){
+            try {
+                img2 = MediaStore.Images.Media.getBitmap(getContentResolver(),data.getData());
+                imageView.setImageBitmap(img2);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    void overPrint(){
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = img1.copy(Bitmap.Config.ARGB_8888, true);
+        canvas.setBitmap(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(20);
+        paint.setColor(getColor(R.color.design_default_color_error));
+        paint.setStrokeWidth(2);
+        int x = canvas.getHeight()/2;
+        int y = canvas.getWidth()/2;
+        canvas.drawText("jay",x,y,paint);
+    }
+    public Bitmap drawTextToBitmap(Context gContext,
+                                   Bitmap bitmap,
+                                   String gText) {
+        Resources resources = gContext.getResources();
+        float scale = resources.getDisplayMetrics().density;
+
+        android.graphics.Bitmap.Config bitmapConfig =
+                bitmap.getConfig();
+        // set default bitmap config if none
+        if(bitmapConfig == null) {
+            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+        }
+        // resource bitmaps are imutable,
+        // so we need to convert it to mutable one
+        bitmap = bitmap.copy(bitmapConfig, true);
+
+        Canvas canvas = new Canvas(bitmap);
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.rgb(61, 61, 61));
+        // text size in pixels
+        paint.setTextSize((int) (14 * scale));
+        // text shadow
+        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(gText, 0, gText.length(), bounds);
+        int x = (bitmap.getWidth() - bounds.width())/2;
+        int y = (bitmap.getHeight() + bounds.height())/2;
+
+        canvas.drawText(gText, x, y, paint);
+
+        return bitmap;
+    }
+}
